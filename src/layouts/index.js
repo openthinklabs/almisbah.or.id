3d488c07ac649c71e36f3308263440b60549dcb8
Wildan Maulana <wildan.m@gmail.com>
2018-05-30 05:56:15 +0700

    update logo

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Yayasan Al-Misbah Ulumul Al-Qur'an" />
     <Navbar />
     <div>{children()}</div>
   </div>
