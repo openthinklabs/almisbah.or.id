196cfb69bdea184c1bea542eb61337d712931296
Wildan Maulana <wildan.m@gmail.com>
2018-07-11 13:10:07 +0700

    update

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -17,7 +17,7 @@ module.exports = {
   userName: "Admin", // Username to display in the author segment.
   userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
   userLocation: "Desa Ciderum, Caringin, Kab. Bogor", // User location to display in the author segment.
-  userAvatar: "/logos/logo-1024.png", // User avatar to display in the author segment.
+  userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
     "Demi Waktu.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
