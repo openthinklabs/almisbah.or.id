8e1b126094abbe63e9373d29c89af0e458ecc62e
Wildan Maulana <wildan.m@gmail.com>
2018-07-11 19:57:44 +0700

    debugging

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,42 +1,62 @@
 module.exports = {
-  siteTitle: "Gatsby Material Starter", // Site title.
-  siteTitleShort: "GM Starter", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: "GatsbyJS Material Starter", // Alternative site title for SEO.
+  siteTitle: "Yayasan Al-Misbah Ulumul Al-Qur'an", // Site title.
+  siteTitleShort: "Yayasan Al-Misbah", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: "Al-Misbah", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://vagr9k.github.io", // Domain of your website without pathPrefix.
-  pathPrefix: "/gatsby-material-starter", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
+  siteUrl: "http://www.almisbah.or.id", // Domain of your website without pathPrefix.
+  pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-material-starter/.
   fixedFooter: false, // Whether the footer component is fixed, i.e. always visible
-  siteDescription: "A GatsbyJS stater with Material design in mind.", // Website description used for RSS feeds/meta description tag.
+  siteDescription: "Yayasan Al-Misbah Ulumul Al-Qur'an.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
-  siteFBAppID: "1825356251115265", // FB Application ID for using app insights
-  siteGATrackingID: "UA-47311644-4", // Tracking code ID for google analytics.
-  disqusShortname: "https-vagr9k-github-io-gatsby-material-starter", // Disqus shortname.
-  postDefaultCategoryID: "Tech", // Default category for posts.
-  dateFromFormat: "YYYY-MM-DD", // Date format used in the frontmatter.
-  dateFormat: "DD/MM/YYYY", // Date format for display.
-  userName: "Material User", // Username to display in the author segment.
+  siteFBAppID: "", // FB Application ID for using app insights
+  siteGATrackingID: "", // Tracking code ID for google analytics.
+  disqusShortname: "", // Disqus shortname.
+  postDefaultCategoryID: "Berita", // Default category for posts.
+  dateFromFormat: "DD-MM-YYYY", // Date format used in the frontmatter.
+  dateFormat: "DD-MM-YYYY", // Date format for display.
+  userName: "Admin", // Username to display in the author segment.
   userTwitter: "", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "North Pole, Earth", // User location to display in the author segment.
+  userLocation: "Desa Ciderum, Caringin, Kab. Bogor", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
-    "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
+    "Demi Waktu.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
     {
-      label: "GitHub",
-      url: "https://github.com/Vagr9K/gatsby-material-starter",
+      label: "Profil",
+      url: "",
       iconClassName: "fa fa-github"
     },
     {
-      label: "Twitter",
-      url: "https://twitter.com/Vagr9K",
+      label: "Kurikulum",
+      url: "",
       iconClassName: "fa fa-twitter"
     },
     {
-      label: "Email",
-      url: "mailto:vagr9k@gmail.com",
+      label: "Galeri",
+      url: "",
       iconClassName: "fa fa-envelope"
-    }
+    },
+    {
+      label: "Alumni",
+      url: "",
+      iconClassName: "fa fa-envelope"
+    },
+    {
+      label: "Artikel",
+      url: "",
+      iconClassName: "fa fa-envelope"
+    },
+    {
+      label: "Program Orang Tua Asuh",
+      url: "",
+      iconClassName: "fa fa-envelope"
+    },
+    {
+      label: "ZISWAF",
+      url: "",
+      iconClassName: "fa fa-envelope"
+    }                
   ],
-  copyright: "Copyright © 2017. Material User" // Copyright string for the footer of the website and RSS feed.
+  copyright: "Copyright © 2018. Yayasan Al-Misbah Ulumul Al-Qur'an" // Copyright string for the footer of the website and RSS feed.
 };
